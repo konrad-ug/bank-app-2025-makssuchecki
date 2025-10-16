39f31de930732a3c39ccc43e750efec340ae9bfb
Maksymilian Suchecki <msuchecki@INF.UG.EDU.PL>
2025-10-16 14:01:45 +0200

    Feature 1-4

--- src/account.py
+++ src/account.py
@@ -1,5 +1,20 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel, promo_kod=None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 0
+        self.balance = 0.0
+        if pesel and len(pesel) == 11:
+            self.pesel = pesel
+        else:
+            self.pesel = "Invalid"
+
+        yy = int(self.pesel[2:])
+        mm = int(self.pesel[2:4])
+        if ((1 <= mm <= 12) and yy <= 60):
+            self.balance = 0.0
+        else:
+            if (promo_kod and promo_kod.startswith("PROM") and len(promo_kod) == len("PROM_XYZ")):
+                self.balance += 50.0
+            
+            
+                
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,35 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "00000000000")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-        assert account.balance == 0
+        assert account.balance == 0.0
+        assert account.pesel == "00000000000"
+
+    def test_pesel_too_short(self):
+        account = Account("John", "Doe", "12345")
+        assert account.pesel == "Invalid"
+    def test_pesel_too_long(self):
+        account = Account("John", "Doe", "12345123451234512345")
+        assert account.pesel == "Invalid"
+    def test_pesel_none(self):
+        account = Account("John", "Doe", None)
+        assert account.pesel == "Invalid"
+
+    def test_correct_promo(self):
+        account = Account("John", "Doe", "79530000000", "PROM_ABC")
+        assert account.balance == 50.0
+    def test_wrong_promo(self):
+        account = Account("John", "Doe", "00000000000", "KODY_XYS")
+        assert account.balance == 0.0
+    def test_too_short_promo(self):
+        account = Account("John", "Doe", "00000000000", "KODY")
+        assert account.balance == 0.0
+
+    def test_senior_nopromo(self):
+        account = Account("John", "Doe", "60010000000", "PROM_KOD")
+        assert account.balance == 0.0
+    def test_notseenior_promo(self):
+        account = Account("John", "Doe", "04290000000", "PROM_KOD")
+        assert account.balance == 50.0
