4c75e4b4d683787f81177a4e3bb54da6dbc7376b
Maksymilian Suchecki <msuchecki@INF.UG.EDU.PL>
2025-10-30 13:26:15 +0100

    hotfixes

--- tests/unit/test_express_transfers.py
+++ tests/unit/test_express_transfers.py
@@ -3,39 +3,39 @@ from src.personal_account import PersonalAccount
 from src.company_account import CompanyAccount 
 
 class TestTransfers:
-    def test_outgoing_express(self):
+    def test_outgoing_persona_express(self):
         account = PersonalAccount("Alice", "Johnson", "12345678901")
         account.balance = 150.0
         account.express_outgoing(50.0)
         assert account.balance == 99.0
 
-    def test_outgoing_express(self):
+    def test_outgoing_copany_express(self):
         account = CompanyAccount("firmex", "1234567899")
         account.balance = 150.0
         account.express_outgoing(50.0)
         assert account.balance == 95.0
 
 
-    def test_outgoing_debit_express(self):
+    def test_outgoing_debit_personal_express(self):
         account = PersonalAccount("Alice", "Johnson", "12345678901")
         account.balance = 50.0
         account.express_outgoing(50.0)
         assert account.balance == -1.0
 
-    def test_outgoing_debit_express(self):
+    def test_outgoing_debit_company_express(self):
         account = CompanyAccount("firmex", "1234567899")
         account.balance = 50.0
         account.express_outgoing(50.0)
         assert account.balance == -5.0
 
-    def test_incoming_express(self):
+    def test_incoming_personal_express(self):
         account = PersonalAccount("Alice", "Johnson", "12345678901")
         account.balance = 100.0
         account.express_incoming(50.0)
         assert account.balance == 150.0
 
 
-    def test_incoming_express(self):
+    def test_incoming_company_express(self):
         account = CompanyAccount("firmex", "1234567899")
         account.balance = 100.0
         account.express_incoming(50.0)
