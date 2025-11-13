6c699faac8f9084dd55028a939a9bbbc31d560d4
Maks Suchecki <maks.suchecki@gmail.com>
2025-11-13 12:10:02 +0100

    fixes

--- tests/unit/test_company_account.py
+++ tests/unit/test_company_account.py
@@ -11,9 +11,7 @@ class TestCompanyAccount:
         account = CompanyAccount("zwiripiach", "99999")
         assert account.company_name == "zwiripiach" 
         assert account.nip == "Invalid"
-        assert account.balance == 0.0
     def test_nip_too_long(self):
         account = CompanyAccount("plastikowe", "44444444444444")
         assert account.company_name == "plastikowe" 
         assert account.nip == "Invalid"
-        assert account.balance == 0.0
